0895c75d5e073d6dec330d73eebbe510b133db6e
MauWebs <mauriciovidalwebs@gmail.com>
2024-03-01 14:06:35 -0300

    commit-01

---
+++ 01-setup.py
@@ -0,0 +1,16 @@
+# ----> Bienvenidos al curso de Python <---- #
+
+print('Comillas Simples')
+print("Comillas Dobles")
+
+# Escribir comentarios en 1 línea 
+
+'''
+Escribir comentarios en multimples 
+líneas con comillas simples
+'''
+
+"""
+Escribir comentarios en multimples 
+líneas con comillas dobles
+"""
